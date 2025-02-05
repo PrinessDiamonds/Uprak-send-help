e035b3ca23fc85cd2990f855a725b4ecb232de36
PrinessDiamonds <jessica.aliudin@gmail.com>
2025-02-05 23:30:42 +0700

    it kinda follows player

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
@@ -5,6 +7,11 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private Vector3 offset;
     private void Start()
+    {
+        transform.position = player.transform.position + offset;
+    }
+
+    private void Update()
     {
         transform.position = player.transform.position + offset;
     }
