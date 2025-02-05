0e3d995cd974bee1996bf40b432d10945404b46d
PrinessDiamonds <jessica.aliudin@gmail.com>
2025-02-05 22:13:41 +0700

    effective movement coding
    1.40

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,29 +16,29 @@ public class Player : MonoBehaviour
    {
     if(Input.GetKeyDown(KeyCode.W) && !isHopping)
     {
-        animator.SetTrigger("hop");
-        isHopping = true;
         float zDifference = 0;
         if(transform.position.z % 1 != 0)
         {
             zDifference = Mathf.Round(transform.position.z) - transform.position.z;
         }
-    transform.position = (transform.position + new Vector3(1, 0, zDifference));
+        MoveCharacter(new Vector3(1, 0, zDifference));
    }
    else if (Input.GetKeyDown(KeyCode.A) && !isHopping)
    {
-    animator.SetTrigger("hop");
-    isHopping = true;
-    transform.position = (transform.position + new Vector3(0, 0, 1));
+        MoveCharacter(new Vector3(0, 0, 1));
    }
    else if (Input.GetKeyDown(KeyCode.D) && !isHopping)
    {
-    animator.SetTrigger("hop");
-    isHopping = true;
-    transform.position = (transform.position + new Vector3(0, 0, -1));
+        MoveCharacter(new Vector3(0, 0, -1));
    }
    }
    
+   private void MoveCharacter(Vector3 difference)
+   {
+    animator.SetTrigger("hop");
+    isHopping = true;
+    transform.position = (transform.position + difference);
+   }
    public void FinishHop()
    {
     isHopping = false;
