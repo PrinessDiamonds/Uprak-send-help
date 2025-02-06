190dd826c1f52b78e71fb613974dfaae400bcf06
PrinessDiamonds <120299103+PrinessDiamonds@users.noreply.github.com>
2025-02-06 10:10:57 +0700

    made water lower than ground, objects white and trees + bushes
    2.14
    
    camera isnt following, check at home pc

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -7,10 +7,6 @@ public class FollowPlayer : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float smoothness;
-    private void Start()
-    {
-        transform.position = player.transform.position + offset;
-    }
 
     private void Update()
     {
